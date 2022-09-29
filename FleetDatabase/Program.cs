712c01e6e9a8f2ad01457cddbc9a68df65e49ce5
tedwa1998 <twedwards98@gmail.com>
2022-09-29 19:46:17 -0400

    Added fleet db context

--- FleetDatabase/Program.cs
+++ FleetDatabase/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using FleetDatabase.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<FleetDatabaseContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("FleetDatabaseContext") ?? throw new InvalidOperationException("Connection string 'FleetDatabaseContext' not found.")));
 
 var app = builder.Build();
 
