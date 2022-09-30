32116645eba0f40b52a4051c30942732e39ce59a
tedwa1998 <twedwards98@gmail.com>
2022-09-29 21:01:01 -0400

    Added Fleet Init for Driver/User

--- FleetDatabase/Program.cs
+++ FleetDatabase/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using FleetDatabase.Data;
+using FleetDatabase.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,6 +11,13 @@ builder.Services.AddDbContext<FleetDatabaseContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("FleetDatabaseContext") ?? throw new InvalidOperationException("Connection string 'FleetDatabaseContext' not found.")));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
